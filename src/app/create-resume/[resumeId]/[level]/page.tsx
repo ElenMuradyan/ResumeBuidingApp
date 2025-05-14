69a9b9c83e01913c531ba426b946b4e7cbec303c
ElenMuradyan <muradyanelen0701@gmail.com>
2025-05-14 22:06:28 +0400

    feat: add socialSection with Firebase Realtime DB integration

--- src/app/create-resume/[resumeId]/[level]/page.tsx
+++ src/app/create-resume/[resumeId]/[level]/page.tsx
@@ -1,6 +1,7 @@
 import EducationSection from "@/components/resume/EducationSection";
 import ProfileSection from "@/components/resume/PersonalInfo";
 import ProjectSection from "@/components/resume/ProjectsSection";
+import SocialSection from "@/components/resume/SocialSection";
 import { HeroHighlight } from "@/components/ui/hero-highlight";
 
 export default function CreateResume () {
@@ -8,7 +9,7 @@ export default function CreateResume () {
         <HeroHighlight className="w-[100%] h-[100vh]">
         <div className="flex w-[100%] items-center justify-center h-[100vh]">
             <div className="w-[50%]">
-            <ProjectSection />
+            <SocialSection/>
             </div>
             <div className="w-[50%]">
             </div>
---
+++ src/components/resume/SocialSection.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import { Form } from "antd";
+import { useSelector } from "react-redux";
+import { Input } from "../ui/input";
+import { RootState } from "@/state-management/store";
+import { onValue, ref } from "firebase/database";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { realTimeDb } from "@/services/firebase/firebase";
+import { formStyles } from "@/styles/constants";
+import { handleRealTimeChange } from "@/features/resume/PersonalSection/formHandlers";
+
+const SocialSection = () => {
+    const [ form ] = Form.useForm();
+    const [socialSection, setSocialSection] = useState<any>({
+        instagram: '',
+        facebook: '',
+        twitter: '',
+        linkedin: ''
+    });
+    const { userData } = useSelector((state: RootState) => state.userProfile.authUserInfo);
+    const { resumeId, level } = useParams();
+    const { push } = useRouter();
+
+    useEffect(() => {
+        if (userData) {
+            const resumeRef = ref(realTimeDb, `users/${userData.uid}/resumes/${resumeId}/${level}`);
+            const unsubscribe = onValue(resumeRef, (snapshot) => {
+              const data = snapshot.val();
+              if (data) {
+                setSocialSection(data);
+                form.setFieldsValue(data);
+              }
+            });
+        
+            return () => unsubscribe(); 
+          }        
+    }, [userData, resumeId])
+
+    return(
+        <Form 
+        form={form}
+        onFieldsChange={(_, allFields) => userData && handleRealTimeChange({allFields, level, resumeId, userData, push})}
+        initialValues={socialSection}
+        style={formStyles}
+        layout="vertical"
+        > 
+            <h1 className="text-white text-2xl">ADD YOUR PERSONAL INFO</h1>
+            
+            <Form.Item
+                className="formItem"
+                name='instagram'
+                label='Instagram Link'
+                rules={[{
+                    required: true,
+                    message: 'Enter your Instagram link!'
+                }]}
+                >
+                    <Input className="Input" placeholder='Instagram Link' type='text'></Input>
+                </Form.Item>
+                <Form.Item
+                className="formItem"
+                name='facebook'
+                label='Facebook Link'
+                rules={[{
+                    required: true,
+                    message: 'Enter your Facebook link!'
+                }]}
+                >
+                    <Input className="Input" placeholder='Facebook Link' type='text'></Input>
+                </Form.Item>
+                <Form.Item
+                className="formItem"
+                name='twitter'
+                label='Twitter Link'
+                rules={[{
+                    required: true,
+                    message: 'Enter your Twitter link!'
+                }]}
+                >
+                    <Input className="Input" placeholder='Twitter Link' type='text'></Input>
+                </Form.Item>
+                <Form.Item
+                className="formItem"
+                name='linkedin'
+                label='LinkedIn Link'
+                rules={[{
+                    required: true,
+                    message: 'Enter your LinkedIn link!'
+                }]}
+                >
+                    <Input className="Input" placeholder='LinkedIn Link' type='text'></Input>
+                </Form.Item>
+            </Form>
+    )
+}
+
+export default SocialSection;
